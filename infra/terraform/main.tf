2efe54e0474636c8a171231d80df026cd2828348
Rafaela Bernardes Rabelo <rafaelabernardesrabelo@usp.br>
2025-11-15 19:18:31 +0000

    s

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,7 +1,13 @@
 terraform {
-  backend "local" {}
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
-module "resource_group" {
-  source = "./"
+provider "azurerm" {
+  features {}
 }
