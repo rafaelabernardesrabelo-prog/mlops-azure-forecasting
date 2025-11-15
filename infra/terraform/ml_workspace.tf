00e56749c4c296d28c5c86a1a809e0cd115ec555
Rafaela Bernardes Rabelo <rafaelabernardesrabelo@usp.br>
2025-11-15 19:32:06 +0000

    cooomit

--- infra/terraform/ml_workspace.tf
+++ infra/terraform/ml_workspace.tf
@@ -3,7 +3,16 @@ resource "azurerm_machine_learning_workspace" "ml_workspace" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
+  # O Workspace exige estes três IDs:
+  key_vault_id            = azurerm_key_vault.kv.id
+  storage_account_id      = azurerm_storage_account.storage.id
+  application_insights_id = azurerm_application_insights.appi.id
+
   identity {
     type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
   }
 }
