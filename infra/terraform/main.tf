f4efd8aed36c4105f08cb30b21df04013f3aef56
Rafaela Bernardes Rabelo <rafaelabernardesrabelo@usp.br>
2025-11-15 19:23:13 +0000

    tent

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,34 +1,5 @@
-# main.tf
-
 terraform {
+  required_version = ">= 1.0"
+
   backend "local" {}
-}
-
-# Aqui você importa os módulos necessários (exemplo):
-module "resource_group" {
-  source = "./resource_group"
-}
-
-module "storage" {
-  source = "./storage"
-}
-
-module "keyvault" {
-  source = "./keyvault"
-}
-
-module "ml_workspace" {
-  source = "./ml_workspace"
-}
-
-module "compute_cluster" {
-  source = "./compute_cluster"
-}
-
-module "appinsights" {
-  source = "./appinsights"
-}
-
-module "acr" {
-  source = "./acr"
 }
--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,8 +1,4 @@
-# providers.tf
-
 terraform {
-  required_version = ">= 1.4.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
