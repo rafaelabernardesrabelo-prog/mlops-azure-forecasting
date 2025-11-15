bac2da7c765a714e7c2344166132294acb29a76e
Rafaela Bernardes Rabelo <rafaelabernardesrabelo@usp.br>
2025-11-15 22:25:08 +0000

    # Pleasefdfd enter the commit message for your changes. Lines starting

--- infra/terraform/ml_workspace.tf
+++ infra/terraform/ml_workspace.tf
@@ -3,10 +3,13 @@ resource "azurerm_machine_learning_workspace" "ml_workspace" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  # O Workspace exige estes três IDs:
+  # Requisitos
   key_vault_id            = azurerm_key_vault.kv.id
   storage_account_id      = azurerm_storage_account.storage.id
   application_insights_id = azurerm_application_insights.appi.id
+
+  # Habilitar acesso público (necessário para usar o ML Studio no browser)
+  public_network_access_enabled = true
 
   identity {
     type = "SystemAssigned"
