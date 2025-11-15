d3e09a56dbfae67a088580af07a2edf973224b70
Rafaela Bernardes Rabelo <rafaelabernardesrabelo@usp.br>
2025-11-15 23:06:22 +0000

    tem

--- infra/terraform/appinsights.tf
+++ infra/terraform/appinsights.tf
@@ -1,10 +1,16 @@
 resource "azurerm_application_insights" "appi" {
-  name                = "${var.prefix}-appi"
+  name                = var.appi_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
 
   tags = {
     environment = "dev"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      workspace_id
+    ]
   }
 }
